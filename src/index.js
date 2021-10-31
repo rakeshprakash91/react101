d9ebc50629a66bec14c2f5c137c3aecd34718aa8
Rakesh Prakash <rakeshprakash91@outlook.com>
2021-10-31 22:33:20 +0530

    basic working version

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { Provider } from "@reduxjs/toolkit";
 import store from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
